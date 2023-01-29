97f3a84097d6dd7930c71b0e52c61592e95512ee
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2023-01-29 15:38:30 +0900

    Update button.c

--- button.c
+++ button.c
@@ -2,6 +2,11 @@
   Project : 버튼입력
   Author  : Jaesung Oh
   TEXT Encoding : UTF-8
+  
+  Attention
+  This software component is licensed under the BSD 3-Clause License.
+  You may not use this file except in compliance with the License. 
+  You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
   ----------------------------------------------------------------------------*/
 
 #include <stdio.h>
