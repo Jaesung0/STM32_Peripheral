388a4a80d6f63a018a78d22a60c6f223db729cf7
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2023-02-05 15:12:59 +0900

    Update adc_F1.h

--- adc_F1.h
+++ adc_F1.h
@@ -12,10 +12,11 @@ extern "C" {
 #endif
 
 #define ADC1_TRIG_TIM      TIM3   //ACD 트리거 타이머
-#define ADC1_CONV_FREQ     20     //1초당 변환수
-#define ADC1_NBR_OF_CH     3      //채널합계, MX_ADC1_Init(void) 함수의 hadc1.Init.NbrOfConversion 과 동일하게 설정
+#define ADC1_CONV_FREQ     10     //1초당 변환수
+#define ADC1_NBR_OF_CH     5      //채널합계, MX_ADC1_Init(void) 함수의 hadc1.Init.NbrOfConversion 과 동일하게 설정
 #define ADC1_NBR_OF_OVS    5      //오버샘플수
-#define ADC1_VREFINT_RANK  1      //MCU내부 1.2V기준전압 채널의 랭크
+#define ADC1_VREF_VOLT     3300   //VREF+ 전압
+#define ADC1_VREFINT_RANK  1      //MCU내부 1.2V기준전압 채널의 랭크, 0으로 설정하면 ADC1_VREF_VOLT 값으로 계산
 #define ADC1_USE_VOLT_DVI  1      //Voltage divider 을 감안한 전압값 활성화 (adc_XX.c 의 gADC1_VolDvi_Table 을 작성해야됨)
 
 void ADC1_TIM_DMA_Enable(void);
