84cc46db2883504ffaa08419f077206fc145aeb6
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2023-08-31 19:38:04 +0900

    Update button.c

--- button.c
+++ button.c
@@ -11,52 +11,61 @@
 
 #include <stdio.h>
 #include "main.h"
-#include "defines.h"
 #include "button.h"
-#include "cmd_func.h"
 #include "uart_F1.h"
+//#include "defines.h"
+//#include "cmd_func.h"
 
 #ifndef BTN_isIN
  #define BTN_isIN()           !LL_GPIO_IsInputPinSet(B1_GPIO_Port, B1_Pin)
 #endif
 
-#define BTN_LONG_TIME        200 //2초
+//버튼 재입력 지연시간 (10ms단위)
+#define BTN_DelayTime     100 //1.0초
+//버튼 재입력 반복시간 (10ms단위)
+#define BTN_RepeatTime     50 //0.5초
 
-static volatile BtnValue_t gBtnNow, gBtnPast = BTN_RELEASE;
-static volatile uint16_t gBTN_Cnt;
+static volatile BtnValue_t BtnNow, BtnPast = BTN_RELEASE;
+static volatile uint16_t BTN_Cnt;
+static uint8_t BTN_Long;
 
 //10ms주기로 실행, 버튼 입력상태 변동 확인하여 버튼상태 변수값에 저장
 void BTN_TIM10ms_Process(void)
 {
   if( BTN_isIN() )
   {
-    if( (gBtnNow != BTN_LONG) && (gBtnPast != BTN_LONG) )
-      gBTN_Cnt++;
+    BTN_Cnt++;
 
-    if(gBtnNow == BTN_RELEASE) //Button_Loop_Process 진입전에 버튼이 변경된 경우
-      gBTN_Cnt = 0; //RELEASE 가 먼저 처리 되도록 gBTN_Cnt 을 0으로
+    if(BtnNow == BTN_RELEASE) //Button_Loop_Process 진입전에 버튼이 변경된 경우
+      BTN_Cnt = 0; //RELEASE 가 먼저 처리 되도록 BTN_Cnt 을 0으로
 
-    if( (gBTN_Cnt) && (gBtnPast == BTN_RELEASE) ) //버튼 눌림
+    if( (BTN_Cnt) && (BtnPast == BTN_RELEASE) ) //버튼 눌림
     {
-      gBtnNow = BTN_PUSH;
+      BtnNow = BTN_PUSH;
     }
-    else if( (gBTN_Cnt > BTN_LONG_TIME) && (gBtnPast == BTN_PUSH) )
+    else if( (BTN_Cnt > BTN_DelayTime) && (BtnPast == BTN_PUSH) ) //반복 지연시간 경과
     {
-      gBtnNow = BTN_LONG;
+      BtnNow = BTN_LONG;
     }
+    else if( (BTN_Cnt > (BTN_DelayTime + BTN_RepeatTime)) && (BtnNow == BTN_NONE) ) //반복지연 + 반복주기시간 경과
+    {
+      BtnNow = BTN_REPEAT;
+      //BTN_Cnt = BTN_DelayTime; // 반복지연 직후로 변경 => Button_Loop_Process() 에서 처리
+    }
+
   }
   else
   {
-    if(gBtnNow == BTN_PUSH) //Button_Loop_Process 진입전에 버튼이 변경된 경우
+    if(BtnNow == BTN_PUSH) //Button_Loop_Process 진입전에 버튼이 변경된 경우
     {
-      gBtnNow = BTN_NONE; //20ms 내 눌렀다 놓인것으로 무시
-      gBTN_Cnt = 0;
+      BtnNow = BTN_NONE; //20ms 내 눌렀다 놓인것으로 무시
+      BTN_Cnt = 0;
     }
 
-    if( (gBtnPast > BTN_RELEASE) || (gBtnNow > BTN_RELEASE))
+    if( (BtnPast > BTN_RELEASE) || (BtnNow > BTN_RELEASE))
     {
-      gBtnNow = BTN_RELEASE;
-      gBTN_Cnt = 0;
+      BtnNow = BTN_RELEASE;
+      BTN_Cnt = 0;
     }
   }
 }
@@ -64,7 +73,7 @@ void BTN_TIM10ms_Process(void)
 //main.c 에서 반복실행
 void Button_Loop_Process(void)
 {
-  BtnValue_t Btn = gBtnNow; //인터럽트에서 변경될 수 있는 변수는 복사하여 사용
+  BtnValue_t Btn = BtnNow; //인터럽트에서 변경될 수 있는 변수는 복사하여 사용
 
   if(Btn)
   {
@@ -78,30 +87,38 @@ void Button_Loop_Process(void)
         break;
 
       case BTN_LONG:
+        BTN_Long = 1;
         printf("Long Pressing");
         //Event_BTN_LongKey_Press();
         break;
 
+      case BTN_REPEAT:
+        BTN_Cnt = BTN_DelayTime;
+        printf("Long Repeat");
+        //Event_BTN_Repeat();
+        break;
+
       case BTN_RELEASE:
-        if(gBtnPast == BTN_PUSH)
+        if(BTN_Long)
+        {
+          BTN_Long = 0;
+          printf("Released(Long Key)");
+          //Event_BTN_LongKey_Release();
+        }
+        else
         {
           printf("Released(Short Key)");
           //Event_BTN_ShortKey_Release();
-        }
-        else//if(gBtnPast == BTN_LONG)
-        {
-          printf("Released(Long Key)");
-          //Event_BTN_LongKey_Release();
         }
         break;
 
       default:
         break;
     }
-    printf("\r\ncmd>");
+    //printf("\r\ncmd>");
     //print_CmdBuf();
 
-    gBtnPast = Btn;
-    gBtnNow = BTN_NONE;
+    BtnPast = Btn;
+    BtnNow = BTN_NONE;
   }
 }
