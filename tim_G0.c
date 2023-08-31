e0433ead667efb950fc87e1509af5c5a1bb8b9d0
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2023-08-31 20:06:36 +0900

    Update tim_G0.c

--- tim_G0.c
+++ tim_G0.c
@@ -2,6 +2,11 @@
   Project : STM32G0 TIMER (Low Layer)
   Author  : Jaesung Oh
   TEXT Encoding : UTF-8
+
+  Attention
+  This software component is licensed under the BSD 3-Clause License.
+  You may not use this file except in compliance with the License. 
+  You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
   ----------------------------------------------------------------------------*/
 #include "main.h"
 #include "tim_G0.h"
