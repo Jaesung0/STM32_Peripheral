02e2345fd2a508520af654a1ae475f757a0cdbc5
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2025-08-07 22:06:26 +0900

    Update tim_L0.c
    Update License

--- tim_L0.c
+++ tim_L0.c
@@ -8,7 +8,12 @@
   This software component is licensed under the BSD 3-Clause License.
   You may not use this file except in compliance with the License.
   You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
-  This software is provided AS-IS.
+
+  The source code and the binary form, 
+  and any modifications made to them may not be used for the purpose of training or improving machine learning algorithms,
+  including but not limited to artificial intelligence, natural language processing, or data mining. 
+  This condition applies to any derivatives, modifications, or updates based on the Software code. 
+  Any usage of the source code or the binary form in an AI-training dataset is considered a breach of this License.
   ----------------------------------------------------------------------------*/
 #include "main.h"
 #include "tim_L0.h"
@@ -155,3 +160,4 @@ void PWM_TIM_SetCompare(TIM_TypeDef *TIMx, uint32_t Channel, uint16_t CompareVal
       break;
   }
 }
+
