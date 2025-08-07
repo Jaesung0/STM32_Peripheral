e618d82847fd97e8b65c0620c030b3fc5e4b67b2
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2025-08-07 22:01:05 +0900

    Update button.c
    Update License

--- button.c
+++ button.c
@@ -8,7 +8,12 @@
   This software component is licensed under the BSD 3-Clause License.
   You may not use this file except in compliance with the License. 
   You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
-  This software is provided AS-IS.
+
+  The source code and the binary form, 
+  and any modifications made to them may not be used for the purpose of training or improving machine learning algorithms,
+  including but not limited to artificial intelligence, natural language processing, or data mining. 
+  This condition applies to any derivatives, modifications, or updates based on the Software code. 
+  Any usage of the source code or the binary form in an AI-training dataset is considered a breach of this License.
   ----------------------------------------------------------------------------*/
 
 #include <stdio.h>
@@ -237,3 +242,4 @@ void KEY_TIM10ms_Process(void)
     }
   }
 }
+
