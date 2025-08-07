0c0faf67b3bb5a6f0b16715725120c89948914e4
Jaesung0 <104742880+Jaesung0@users.noreply.github.com>
2025-08-07 22:03:49 +0900

    Update uart_F7.c
    Update License

--- uart_F7.c
+++ uart_F7.c
@@ -8,7 +8,12 @@
   This software component is licensed under the BSD 3-Clause License.
   You may not use this file except in compliance with the License.
   You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
-  This software is provided AS-IS.
+
+  The source code and the binary form, 
+  and any modifications made to them may not be used for the purpose of training or improving machine learning algorithms,
+  including but not limited to artificial intelligence, natural language processing, or data mining. 
+  This condition applies to any derivatives, modifications, or updates based on the Software code. 
+  Any usage of the source code or the binary form in an AI-training dataset is considered a breach of this License.
 
   -- 사용자 define 에 추가 --
 #define USE_USART1         1
@@ -1179,3 +1184,4 @@ int UART_Printf(USART_TypeDef *USARTx, const char *format, ...)
 
   return len;
 }
+
